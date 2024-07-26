7d7281ec17e332f216ccbec08ccbf5cd4509df7f
Samuel Wilson <samWson@users.noreply.github.com>
2024-07-26 13:23:28 +1200

    Read a single character from stdin continuously until the program stops

--- kilo.go
+++ kilo.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+
+	for {
+		c := make([]byte, 1)
+
+		bytesRead, err := os.Stdin.Read(c)
+		if bytesRead != 1 {
+			os.Exit(0)
+		}
+
+		if err != nil {
+			if err == io.EOF {
+				os.Exit(0)
+			}
+
+			fmt.Println("Error")
+			os.Exit(1)
+		}
+
+		fmt.Printf("%v", (string(c)))
+	}
 }
